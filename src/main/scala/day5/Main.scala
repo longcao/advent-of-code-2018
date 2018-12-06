9be62e2e8eaad9158bab9bba4941dddb2da9ee74
Long Cao <longcao@gmail.com>
2018-12-06 17:34:29 -0500

    day 5 part 2

--- src/main/scala/day5/Main.scala
+++ src/main/scala/day5/Main.scala
@@ -15,12 +15,13 @@ object Main extends IOApp {
   def run(args: List[String]): IO[ExitCode] = {
     args(0) match {
       case "part1" => part1
+      case "part2" => part2
       case _       => IO.pure(ExitCode.Error)
     }
   }
 
-  def part1 = input.compile
-    .fold(Chain.empty[Char]) { (chain, char) =>
+  def react(polymer: List[Char]): Chain[Char] = {
+    polymer.foldLeft(Chain.empty[Char]) { (chain, char) =>
       chain.uncons match {
         case Some((head, tail)) =>
           // compare their decimal values, alpha chars are 32 apart from the other case
@@ -33,7 +34,25 @@ object Main extends IOApp {
           chain.prepend(char)
       }
     }
+  }
+
+  def part1 = input.compile.toList
+    .map(polymer => react(polymer))
     .flatTap(result => IO(println(s"Final result: ${result.length}")))
+    .as(ExitCode.Success)
+
+  def part2 = ('a' to 'z').toList
+    .map { char =>
+      input.compile.toList.map { polymer =>
+        val filtered = polymer.filterNot { unit =>
+          unit == char || unit == char.toUpper
+        }
+
+        react(filtered).length
+      }
+    }
+    .sequence
+    .flatTap(reacted => IO(println(reacted.min)))
     .as(ExitCode.Success)
 
   val input: Stream[IO, Char] =
